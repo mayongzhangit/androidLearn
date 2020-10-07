ae5264324a72f722913f0cc8d9b475eafddf9a63
yzMa <2641007740@qq.com>
2020-10-07 17:37:32 +0800

    android layout

---
+++ ch3-framelayout-color/src/main/java/com/myz/android/ch3_framelayout_color/MainActivity.java
@@ -0,0 +1,14 @@
+package com.myz.android.ch3_framelayout_color;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
